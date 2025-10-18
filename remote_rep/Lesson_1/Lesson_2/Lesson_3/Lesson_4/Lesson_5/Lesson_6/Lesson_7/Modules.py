ac6909220bbf578a92bde9989c4ba6576bd6dd22
TeddyZBunny <aaa_sk@mail.ru>
2025-10-18 17:13:39 +0400

    Added modules

---
+++ remote_rep/Lesson_1/Lesson_2/Lesson_3/Lesson_4/Lesson_5/Lesson_6/Lesson_7/Modules.py
@@ -0,0 +1,15 @@
+# Чтобы установить любой модуль - нам нужна команда pip install <module_name>
+# Репозиторий для всех сторонних модулей питона - pypi.org
+import random # Модуль рандомных функцих
+import sys
+import mymodule
+
+asist(1)
+
+print(sys.platform)
+
+print(random.randint(0,5))
+print(random.randrange(6,8)) #рандрендж до невключительно
+
+users=['Aleks','Antont','Sergey']
+print(random.choice(users))
