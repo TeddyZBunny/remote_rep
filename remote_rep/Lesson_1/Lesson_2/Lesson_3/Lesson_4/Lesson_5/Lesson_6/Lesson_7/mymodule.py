7688bf60f4b0aa9aff12e93437fe06d2a559b961
TeddyZBunny <aaa_sk@mail.ru>
2025-10-19 15:30:33 +0400

    dsfadfadsf

---
+++ remote_rep/Lesson_1/Lesson_2/Lesson_3/Lesson_4/Lesson_5/Lesson_6/Lesson_7/mymodule.py
@@ -0,0 +1,12 @@
+def asist(a):
+    while a == 'True':
+            return True
+            break
+    else:
+            return False
+
+def very_usefulfunction_and_very_long_fonc():
+    print('What do you want')
+
+
+very_usefulfunction_and_very_long_fonc()
